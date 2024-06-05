9c3ac10f029a3aeabacdbe2fff7fc9a6c0c5d29f
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-06-05 14:24:23 -0400

    chore: copy pcf files

---
+++ internal/pcf/factory/factory.go
@@ -0,0 +1,51 @@
+// Copyright 2019 free5GC.org
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/*
+ * PCF Configuration Factory
+ */
+
+package factory
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/yeastengine/canard/internal/pcf/logger"
+	"gopkg.in/yaml.v2"
+)
+
+var PcfConfig Config
+
+// TODO: Support configuration update from REST api
+func InitConfigFactory(f string) error {
+	if content, err := os.ReadFile(f); err != nil {
+		return err
+	} else {
+		PcfConfig = Config{}
+
+		if yamlErr := yaml.Unmarshal(content, &PcfConfig); yamlErr != nil {
+			return yamlErr
+		}
+		if PcfConfig.Configuration.WebuiUri == "" {
+			PcfConfig.Configuration.WebuiUri = "webui:9876"
+		}
+	}
+
+	return nil
+}
+
+func CheckConfigVersion() error {
+	currentVersion := PcfConfig.GetVersion()
+
+	if currentVersion != PCF_EXPECTED_CONFIG_VERSION {
+		return fmt.Errorf("config version is [%s], but expected is [%s].",
+			currentVersion, PCF_EXPECTED_CONFIG_VERSION)
+	}
+
+	logger.CfgLog.Infof("config version [%s]", currentVersion)
+
+	return nil
+}
---
+++ internal/pcf/pcf.go
@@ -0,0 +1,145 @@
+package pcf
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"github.com/yeastengine/canard/internal/pcf/logger"
+	"github.com/yeastengine/canard/internal/pcf/service"
+)
+
+var PCF = &service.PCF{}
+
+var appLog *logrus.Entry
+
+const (
+	dBName     = "sdcore_pcf"
+	SBI_PORT   = "29518"
+	NGAPP_PORT = "38412"
+)
+
+func init() {
+	appLog = logger.AppLog
+}
+
+func getContext(mongoDBURL string, nrfURL string, webuiURL string) (*cli.Context, error) {
+	flagSet := flag.NewFlagSet("test", flag.ContinueOnError)
+	flagSet.String("pcfcfg", "", "PCF configuration")
+	app := cli.NewApp()
+	appLog.Infoln(app.Name)
+	c := cli.NewContext(app, flagSet, nil)
+	pcfConfig := fmt.Sprintf(`
+configuration:
+  pcfDBName: %s
+  pcfName: PCF
+  debugProfilePort: 5001
+  enableDBStore: false
+  enableSctpLb: false
+  mongodb:
+    url: %s
+  networkFeatureSupport5GS:
+    emc: 0
+    emcN3: 0
+    emf: 0
+    enable: true
+    imsVoPS: 0
+    iwkN26: 0
+    mcsi: 0
+    mpsi: 0
+  ngapIpList:
+    - 0.0.0.0
+  ngappPort: %s
+  nrfUri: %s
+  webuiUri: %s
+  sbi:
+    bindingIPv4: 0.0.0.0
+    port: %s
+    registerIPv4: 0.0.0.0
+    scheme: http
+  sctpGrpcPort: 9000
+  serviceNameList:
+    - npcf-comm
+    - npcf-evts
+    - npcf-mt
+    - npcf-loc
+    - npcf-oam
+  supportDnnList:
+    - internet
+  security:
+    integrityOrder:
+      - NIA1
+      - NIA2
+    cipheringOrder:
+      - NEA0
+  networkName:
+    full: SDCORE5G
+    short: SDCORE
+  t3502Value: 720
+  t3512Value: 3600
+  t3513:
+    enable: true
+    expireTime: 6s
+    maxRetryTimes: 4
+  t3522:
+    enable: true
+    expireTime: 6s
+    maxRetryTimes: 4
+  t3550:
+    enable: true
+    expireTime: 6s
+    maxRetryTimes: 4
+  t3560:
+    enable: true
+    expireTime: 6s
+    maxRetryTimes: 4
+  t3565:
+    enable: true
+    expireTime: 6s
+    maxRetryTimes: 4
+info:
+  description: PCF initial configuration
+  version: 1.0.0
+`, dBName, mongoDBURL, NGAPP_PORT, nrfURL, webuiURL, SBI_PORT)
+	tmpFile, err := os.CreateTemp("", "pcfcfg-*.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp file: %w", err)
+	}
+
+	_, err = tmpFile.Write([]byte(pcfConfig))
+	if err != nil {
+		tmpFile.Close()
+		os.Remove(tmpFile.Name())
+		return nil, fmt.Errorf("failed to write to temp file: %w", err)
+	}
+
+	if err = tmpFile.Close(); err != nil {
+		os.Remove(tmpFile.Name())
+		return nil, fmt.Errorf("failed to close temp file: %w", err)
+	}
+
+	err = c.Set("pcfcfg", tmpFile.Name())
+	if err != nil {
+		os.Remove(tmpFile.Name())
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func Start(mongoDBURL string, nrfURL string, webuiURL string) error {
+	c, err := getContext(mongoDBURL, nrfURL, webuiURL)
+	if err != nil {
+		logger.CfgLog.Errorf("%+v", err)
+		return fmt.Errorf("failed to get context")
+	}
+	err = PCF.Initialize(c)
+	if err != nil {
+		logger.CfgLog.Errorf("%+v", err)
+		return fmt.Errorf("failed to initialize")
+	}
+	go PCF.Start()
+	return nil
+}
