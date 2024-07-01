eeaccf571618cc14e0342f44789b175c4d1da120
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-07-01 17:10:06 -0400

    chore: remove code related to upf-adapter (#70)

--- internal/smf/pfcp/upf/upf.go
+++ internal/smf/pfcp/upf/upf.go
@@ -21,7 +21,7 @@ func InitPfcpHeartbeatRequest(userplane *context.UserPlaneInformation) {
 		for _, upf := range userplane.UPFs {
 			upf.UPF.UpfLock.Lock()
 			if (upf.UPF.UPFStatus == context.AssociatedSetUpSuccess) && upf.UPF.NHeartBeat < maxHeartbeatRetry {
-				err := message.SendHeartbeatRequest(upf.NodeID, upf.Port) // needs lock in sync rsp(adapter mode)
+				err := message.SendHeartbeatRequest(upf.NodeID, upf.Port)
 				if err != nil {
 					logger.PfcpLog.Errorf("send pfcp heartbeat request failed: %v for UPF[%v, %v]: ", err, upf.NodeID, upf.NodeID.ResolveNodeIdToIp())
 				} else {
