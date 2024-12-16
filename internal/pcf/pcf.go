56026edb64ee12220bcc657d8a47ad7549dc5b5a
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-12-16 07:20:10 -0500

    chore: remove unnecessary event dependency (#257)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/pcf/pcf.go
+++ internal/pcf/pcf.go
@@ -7,15 +7,10 @@ import (
 	"github.com/omec-project/openapi/models"
 	"github.com/yeastengine/ella/internal/db"
 	"github.com/yeastengine/ella/internal/pcf/context"
-	"github.com/yeastengine/ella/internal/pcf/internal/notifyevent"
 	"github.com/yeastengine/ella/internal/util/idgenerator"
 )
 
 func Start(dbInstance *db.Database) error {
-	err := notifyevent.RegisterNotifyDispatcher()
-	if err != nil {
-		return err
-	}
 	context := context.PCF_Self()
 	context.TimeFormat = "2006-01-02 15:04:05"
 	context.PcfSuppFeats = make(map[models.ServiceName]openapi.SupportedFeature)
