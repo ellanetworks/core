3a30965b4625ea91d43af8de4d4c685785bd527d
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-01-04 08:23:22 -0500

    chore: re-add license headers
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/amf/gmm/mock_gmm.go
+++ internal/amf/gmm/mock_gmm.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Ella Networks
+// SPDX-FileCopyrightText: 2022 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package gmm
 
 import (
--- internal/amf/ngap/util/ngap_util.go
+++ internal/amf/ngap/util/ngap_util.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Ella Networks
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package util
 
 import (
