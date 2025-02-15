e0bd8cd86fea2a2fb948f233de12420999e21d5e
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-02-15 11:43:59 -0500

    chore: remove unused ebpf code (#424)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/upf/ebpf/objects.go
+++ internal/upf/ebpf/objects.go
@@ -21,9 +21,6 @@ import (
 //
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cflags "$BPF_CFLAGS" -target bpf IpEntrypoint 	xdp/n3n6_entrypoint.c -- -I. -O2 -Wall -g
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpf ZeroEntrypoint 	xdp/zero_entrypoint.c -- -I. -O2 -Wall
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpf N3Entrypoint 	xdp/n3_entrypoint.c -- -I. -O2 -Wall
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpf N6Entrypoint 	xdp/n6_entrypoint.c -- -I. -O2 -Wall
 
 type BpfObjects struct {
 	IpEntrypointObjects
@@ -89,86 +86,6 @@ func CloseAllObjects(closers ...io.Closer) error {
 			return err
 		}
 	}
-	return nil
-}
-
-func ResizeEbpfMap(eMap **ebpf.Map, eProg *ebpf.Program, newSize uint32) error {
-	mapInfo, err := (*eMap).Info()
-	if err != nil {
-		logger.UpfLog.Infof("Failed get ebpf map info: %s", err)
-		return err
-	}
-	mapInfo.MaxEntries = newSize
-	// Create a new MapSpec using the information from MapInfo
-	mapSpec := &ebpf.MapSpec{
-		Name:       mapInfo.Name,
-		Type:       mapInfo.Type,
-		KeySize:    mapInfo.KeySize,
-		ValueSize:  mapInfo.ValueSize,
-		MaxEntries: mapInfo.MaxEntries,
-		Flags:      mapInfo.Flags,
-	}
-	if err != nil {
-		logger.UpfLog.Infof("Failed to close old ebpf map: %s, %+v", err, *eMap)
-		return err
-	}
-
-	// Unpin the old map
-	err = (*eMap).Unpin()
-	if err != nil {
-		logger.UpfLog.Infof("Failed to unpin old ebpf map: %s, %+v", err, *eMap)
-		return err
-	}
-
-	// Close the old map
-	err = (*eMap).Close()
-	if err != nil {
-		logger.UpfLog.Infof("Failed to close old ebpf map: %s, %+v", err, *eMap)
-		return err
-	}
-
-	// Old map will be garbage collected sometime after this point
-
-	*eMap, err = ebpf.NewMapWithOptions(mapSpec, ebpf.MapOptions{})
-	if err != nil {
-		logger.UpfLog.Infof("Failed to create resized ebpf map: %s", err)
-		return err
-	}
-	err = eProg.BindMap(*eMap)
-	if err != nil {
-		logger.UpfLog.Infof("Failed to bind resized ebpf map: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (bpfObjects *BpfObjects) ResizeAllMaps(qerMapSize uint32, farMapSize uint32, pdrMapSize uint32) error {
-	// QER
-	if err := ResizeEbpfMap(&bpfObjects.QerMap, bpfObjects.UpfIpEntrypointFunc, qerMapSize); err != nil {
-		logger.UpfLog.Infof("Failed to resize QER map: %s", err)
-		return err
-	}
-
-	// FAR
-	if err := ResizeEbpfMap(&bpfObjects.FarMap, bpfObjects.UpfIpEntrypointFunc, farMapSize); err != nil {
-		logger.UpfLog.Infof("Failed to resize FAR map: %s", err)
-		return err
-	}
-
-	// PDR
-	if err := ResizeEbpfMap(&bpfObjects.PdrMapDownlinkIp4, bpfObjects.UpfIpEntrypointFunc, pdrMapSize); err != nil {
-		logger.UpfLog.Infof("Failed to resize PDR map: %s", err)
-		return err
-	}
-	if err := ResizeEbpfMap(&bpfObjects.PdrMapDownlinkIp6, bpfObjects.UpfIpEntrypointFunc, pdrMapSize); err != nil {
-		logger.UpfLog.Infof("Failed to resize PDR map: %s", err)
-		return err
-	}
-	if err := ResizeEbpfMap(&bpfObjects.PdrMapUplinkIp4, bpfObjects.UpfIpEntrypointFunc, pdrMapSize); err != nil {
-		logger.UpfLog.Infof("Failed to resize PDR map: %s", err)
-		return err
-	}
-
 	return nil
 }
 
