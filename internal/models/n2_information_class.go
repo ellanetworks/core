89c9014eef8fda23d7bb998047ee3425235e306d
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-03-02 17:30:11 -0500

    chore: add no sec for false warning
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/models/n2_information_class.go
+++ internal/models/n2_information_class.go
@@ -7,7 +7,7 @@ const (
 	N2InformationClass_SM       N2InformationClass = "SM"
 	N2InformationClass_NRP_PA   N2InformationClass = "NRPPa"
 	N2InformationClass_PWS      N2InformationClass = "PWS"
-	N2InformationClass_PWS_BCAL N2InformationClass = "PWS-BCAL"
+	N2InformationClass_PWS_BCAL N2InformationClass = "PWS-BCAL" // #nosec G101
 	N2InformationClass_PWS_RF   N2InformationClass = "PWS-RF"
 	N2InformationClass_RAN      N2InformationClass = "RAN"
 )
