dbb3fa2ed0e8bf399f36ded065961339786bd5ec
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-03-04 17:36:40 -0500

    chore: remove unneeded models
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/models/sm_context_update_error.go
+++ internal/models/sm_context_update_error.go
@@ -1,9 +1,8 @@
 package models
 
-type SmContextUpdateError struct {
-	Error        *ProblemDetails
-	N1SmMsg      *RefToBinaryData
-	N2SmInfo     *RefToBinaryData
-	N2SmInfoType N2SmInfoType
-	UpCnxState   UpCnxState
-}
+// type SmContextUpdateError struct {
+// 	N1SmMsg      *RefToBinaryData
+// 	N2SmInfo     *RefToBinaryData
+// 	N2SmInfoType N2SmInfoType
+// 	UpCnxState   UpCnxState
+// }
