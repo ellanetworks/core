dcaf654c55a6aca7175682ad7f496aa55ed20316
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-11-12 06:46:15 -0500

    chore: revert to make sure issues is present
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -310,9 +310,9 @@ func Validate(filePath string) (Config, error) {
 	}
 
 	if c.XDP.AttachMode == AttachModeNative {
-		config.Interfaces.N3.VlanConfig, err = GetVLANConfigForInterfaceFunc(n3InterfaceName)
+		config.Interfaces.N3.VlanConfig, err = GetVLANConfigForInterfaceFunc(c.Interfaces.N3.Name)
 		if err != nil {
-			return Config{}, fmt.Errorf("cannot get vlan config for interface %s: %w", n3InterfaceName, err)
+			return Config{}, fmt.Errorf("cannot get vlan config for interface %s: %w", c.Interfaces.N3.Name, err)
 		}
 		config.Interfaces.N6.VlanConfig, err = GetVLANConfigForInterfaceFunc(c.Interfaces.N6.Name)
 		if err != nil {
