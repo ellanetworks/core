409f12fedac241f48a274a785efb60e5e4e4a2ad
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-11-12 06:41:01 -0500

    chore: validating ella-core-tester
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- integration/gnbsim_test.go
+++ integration/gnbsim_test.go
@@ -15,6 +15,8 @@ func TestIntegrationGnbsim(t *testing.T) {
 		t.Skip("skipping integration tests, set environment variable INTEGRATION")
 	}
 
+	t.Skip("Validating Ella Core Tester")
+
 	ctx := context.Background()
 
 	dockerClient, err := NewDockerClient()
--- integration/ueransim_test.go
+++ integration/ueransim_test.go
@@ -15,6 +15,8 @@ func TestIntegrationUERANSIM(t *testing.T) {
 		t.Skip("skipping integration tests, set environment variable INTEGRATION")
 	}
 
+	t.Skip("Validating Ella Core Tester")
+
 	testCases := []struct {
 		name string
 		nat  bool
