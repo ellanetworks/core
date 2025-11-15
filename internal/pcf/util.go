72a1d01bd22f7fd57810fa862966a68b699cb7e1
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-11-15 07:41:22 -0500

    chore: remove unused function
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/pcf/util.go
+++ internal/pcf/util.go
@@ -6,29 +6,9 @@ package pcf
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/ellanetworks/core/internal/logger"
 	"github.com/ellanetworks/core/internal/models"
-	"go.uber.org/zap"
 )
-
-// Convert Snssai form models to hexString(sst(2)+sd(6))
-func SnssaiModelsToHex(snssai models.Snssai) string {
-	// Format sst as a two-digit hex number.
-	sst := fmt.Sprintf("%02x", snssai.Sst)
-	combined := sst + snssai.Sd
-	logger.SmfLog.Warn("TO DELETE: Combined SNSSAI in hex", zap.Int("sst", int(snssai.Sst)), zap.String("sd", snssai.Sd), zap.String("snssai", combined))
-
-	// Remove all leading '0' characters.
-	result := strings.TrimLeft(combined, "0")
-
-	// In case the string was all zeros, return "0" instead of an empty string.
-	if result == "" {
-		return "0"
-	}
-	return result
-}
 
 // GetSMPolicyDnnData returns SMPolicyDnnData derived from SmPolicy data which snssai and dnn match
 func GetSMPolicyDnnData(data models.SmPolicyData, snssai *models.Snssai, dnn string) (*models.SmPolicyDnnData, error) {
