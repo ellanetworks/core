40744f51ecb68eadef47c59a7d5bbd9cf33b6229
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-11-21 15:08:31 -0500

    chore: separate amf id in actual 3gpp fields
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/decoder/ngap/amf_status_indication_test.go
+++ internal/decoder/ngap/amf_status_indication_test.go
@@ -82,7 +82,15 @@ func TestDecodeNGAPMessage_AMFStatusIndication(t *testing.T) {
 		t.Errorf("expected MNC=01, got %s", guami.PLMNID.Mnc)
 	}
 
-	if guami.AMFID != "cafe00" {
-		t.Errorf("expected AMFID=cafe00, got %s", guami.AMFID)
+	if guami.AMFRegionID != "ca" {
+		t.Errorf("expected AMFRegionID=ca, got %s", guami.AMFRegionID)
+	}
+
+	if guami.AMFSetID != "fe0" {
+		t.Errorf("expected AMFSetID=fe0, got %s", guami.AMFSetID)
+	}
+
+	if guami.AMFPointer != "00" {
+		t.Errorf("expected AMFPointer=00, got %s", guami.AMFPointer)
 	}
 }
