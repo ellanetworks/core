b0df495be208b21e8a6d3e0b3edb4f5a9fa5e4a6
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2026-01-21 11:30:42 -0500

    chore: Add unit tests for handleIdentityResponse

--- internal/amf/nas/gmm/handle_identity_response_test.go
+++ internal/amf/nas/gmm/handle_identity_response_test.go
@@ -1,11 +1,18 @@
 package gmm
 
 import (
+	"context"
+	"encoding/hex"
 	"fmt"
 	"testing"
 
 	amfContext "github.com/ellanetworks/core/internal/amf/context"
+	"github.com/ellanetworks/core/internal/db"
+	"github.com/ellanetworks/core/internal/models"
+	"github.com/free5gc/nas"
 	"github.com/free5gc/nas/nasMessage"
+	"github.com/free5gc/nas/nasType"
+	"github.com/free5gc/nas/security"
 )
 
 type UpdateInputs struct {
@@ -213,4 +220,452 @@ func TestUpdateUeIdentity(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHandleIdentityResponse_InvalidStateError(t *testing.T) {
+	testcases := []amfContext.StateType{amfContext.Deregistered, amfContext.Registered, amfContext.SecurityMode}
+
+	for _, tc := range testcases {
+		t.Run(string(tc), func(t *testing.T) {
+			err := handleIdentityResponse(context.TODO(), &amfContext.AMF{}, &amfContext.AmfUe{State: tc}, &nasMessage.IdentityResponse{})
+			if err == nil {
+				t.Fatalf("expected an state mismatch error, got no error")
+			}
+		})
+	}
+}
+
+func TestHandleIdentityResponse_AuthenticationProcess_AuthenticationRequest(t *testing.T) {
+	amf := &amfContext.AMF{
+		DBInstance: &FakeDBInstance{
+			Operator: &db.Operator{
+				Mcc:           "001",
+				Mnc:           "01",
+				Sst:           1,
+				SupportedTACs: "[\"1\"]",
+			},
+		},
+		Ausf: &FakeAusf{
+			AvKgAka: &models.Av5gAka{
+				Rand: hex.EncodeToString(make([]byte, 16)),
+				Autn: hex.EncodeToString(make([]byte, 16)),
+			},
+			Supi:  "imsi-001019756139935",
+			Kseaf: "testkey",
+		},
+		UEs: make(map[string]*amfContext.AmfUe),
+	}
+
+	ue, ngapSender, err := buildUeAndRadio()
+	if err != nil {
+		t.Fatalf("could not create UE and radio: %v", err)
+	}
+
+	ue.Suci = ""
+	ue.Supi = ""
+	ue.State = amfContext.Authentication
+	ue.MacFailed = false
+	ue.Tai = ue.RanUe.Tai
+
+	m := buildTestIdentityResponseMessage()
+
+	err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+	if err != nil {
+		t.Fatalf("expected no errors but got: %v", err)
+	}
+
+	if len(ngapSender.SentDownlinkNASTransport) != 1 {
+		t.Fatalf("should have sent a Downlink NAS Transport message")
+	}
+
+	resp := ngapSender.SentDownlinkNASTransport[0]
+	nm := new(nas.Message)
+	nm.SecurityHeaderType = nas.GetSecurityHeaderType(resp.NasPdu) & 0x0f
+
+	if nm.SecurityHeaderType != nas.SecurityHeaderTypePlainNas {
+		t.Fatalf("expected a plain NAS message")
+	}
+
+	err = nm.PlainNasDecode(&resp.NasPdu)
+	if err != nil {
+		t.Fatalf("could not decode plain NAS message")
+	}
+
+	if nm.GmmHeader.GetMessageType() != nas.MsgTypeAuthenticationRequest {
+		t.Fatalf("expected an authentication request message, got '%v'", nm.GmmHeader.GetMessageType())
+	}
+}
+
+func TestHandleIdentityResponse_AuthenticationProcess_AuthenticationError(t *testing.T) {
+	amf := &amfContext.AMF{
+		DBInstance: &FakeDBInstance{
+			Operator: &db.Operator{
+				Mcc:           "001",
+				Mnc:           "01",
+				Sst:           1,
+				SupportedTACs: "[\"1\"]",
+			},
+		},
+		Ausf: &FakeAusf{
+			AvKgAka: &models.Av5gAka{
+				Rand: hex.EncodeToString(make([]byte, 16)),
+				Autn: hex.EncodeToString(make([]byte, 16)),
+			},
+			Supi:  "imsi-001019756139935",
+			Kseaf: "testkey",
+		},
+		UEs: make(map[string]*amfContext.AmfUe),
+	}
+
+	ue, ngapSender, err := buildUeAndRadio()
+	if err != nil {
+		t.Fatalf("could not create UE and radio: %v", err)
+	}
+
+	ue.Suci = ""
+	ue.Supi = ""
+	ue.State = amfContext.Authentication
+	ue.MacFailed = false
+	ue.Tai = models.Tai{}
+
+	m := buildTestIdentityResponseMessage()
+
+	expected := "error in authentication procedure: failed to send ue authentication request: tai is not available in UE context"
+
+	err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+	if err == nil {
+		t.Fatalf("expected error but got none")
+	}
+
+	if err.Error() != expected {
+		t.Fatalf("expected error: %v, got %v", expected, err)
+	}
+
+	if len(ngapSender.SentDownlinkNASTransport) != 0 {
+		t.Fatalf("should not have sent a Downlink NAS Transport message")
+	}
+}
+
+func TestHandleIdentityResponse_AuthenticationProcess_RegistrationAccept(t *testing.T) {
+	supi := "imsi-001019756139935"
+	amf := &amfContext.AMF{
+		DBInstance: &FakeDBInstance{
+			Operator: &db.Operator{
+				Mcc:           "001",
+				Mnc:           "01",
+				Sst:           1,
+				SupportedTACs: "[\"1\"]",
+			},
+		},
+		Ausf: &FakeAusf{
+			AvKgAka: &models.Av5gAka{
+				Rand: hex.EncodeToString(make([]byte, 16)),
+				Autn: hex.EncodeToString(make([]byte, 16)),
+			},
+			Supi:  supi,
+			Kseaf: "testkey",
+		},
+		UEs: make(map[string]*amfContext.AmfUe),
+	}
+
+	ue, ngapSender, err := buildUeAndRadio()
+	if err != nil {
+		t.Fatalf("could not create UE and radio: %v", err)
+	}
+
+	ue.Suci = "testsuci"
+	ue.Supi = supi
+	ue.State = amfContext.Authentication
+	ue.MacFailed = false
+	ue.Tai = ue.RanUe.Tai
+	ue.SecurityContextAvailable = true
+	ue.NgKsi.Ksi = 1
+	key := [16]uint8{0x0D, 0x0E, 0x0A, 0x0D, 0x0B, 0x0E, 0x0E, 0x0F, 0x0F, 0x0E, 0x0E, 0x0D, 0x0C, 0x0A, 0x0F, 0x0E}
+	algo := security.AlgCiphering128NEA2
+	ue.KnasEnc = key
+	ue.KnasInt = key
+	ue.CipheringAlg = algo
+	ue.IntegrityAlg = security.AlgIntegrity128NIA0
+
+	registrationRequest, err := buildTestRegistrationRequestMessage(algo, &key, ue.ULCount.Get())
+	if err != nil {
+		t.Fatalf("could not build registration request message: %v", err)
+	}
+
+	ue.RegistrationRequest = registrationRequest.RegistrationRequest
+	ue.RegistrationType5GS = nasMessage.RegistrationType5GSInitialRegistration
+
+	m := buildTestIdentityResponseMessage()
+
+	err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+	if err != nil {
+		t.Fatalf("expected no errors but got: %v", err)
+	}
+
+	if len(ngapSender.SentDownlinkNASTransport) != 1 {
+		t.Fatalf("should have sent a Downlink NAS Transport message")
+	}
+
+	resp := ngapSender.SentDownlinkNASTransport[0]
+	nm := new(nas.Message)
+	nm.SecurityHeaderType = nas.GetSecurityHeaderType(resp.NasPdu) & 0x0f
+
+	payload := make([]byte, len(resp.NasPdu))
+	copy(payload, resp.NasPdu)
+	payload = payload[7:]
+
+	if nm.SecurityHeaderType != nas.SecurityHeaderTypeIntegrityProtectedAndCiphered {
+		t.Fatalf("expected a protected and ciphered NAS message")
+	}
+
+	if err := security.NASEncrypt(ue.CipheringAlg, ue.KnasEnc, ue.ULCount.Get(), security.Bearer3GPP, security.DirectionDownlink, payload); err != nil {
+		t.Fatalf("could not decrypt NAS message: %v", err)
+	}
+
+	err = nm.PlainNasDecode(&payload)
+	if err != nil {
+		t.Fatalf("could not decode ciphered NAS message: %v", err)
+	}
+
+	if nm.GmmHeader.GetMessageType() != nas.MsgTypeRegistrationAccept {
+		t.Fatalf("expected a registration accept message, got '%v'", nm.GmmHeader.GetMessageType())
+	}
+}
+
+func TestHandleIdentityResponse_ContextSetup_RegistrationAccept(t *testing.T) {
+	testcases := []uint8{
+		nasMessage.RegistrationType5GSInitialRegistration,
+		nasMessage.RegistrationType5GSMobilityRegistrationUpdating,
+		nasMessage.RegistrationType5GSPeriodicRegistrationUpdating,
+	}
+
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+			supi := "imsi-001019756139935"
+			amf := &amfContext.AMF{
+				DBInstance: &FakeDBInstance{
+					Operator: &db.Operator{
+						Mcc:           "001",
+						Mnc:           "01",
+						Sst:           1,
+						SupportedTACs: "[\"1\"]",
+					},
+				},
+				Ausf: &FakeAusf{
+					AvKgAka: &models.Av5gAka{
+						Rand: hex.EncodeToString(make([]byte, 16)),
+						Autn: hex.EncodeToString(make([]byte, 16)),
+					},
+					Supi:  supi,
+					Kseaf: "testkey",
+				},
+				UEs: make(map[string]*amfContext.AmfUe),
+			}
+
+			ue, ngapSender, err := buildUeAndRadio()
+			if err != nil {
+				t.Fatalf("could not create UE and radio: %v", err)
+			}
+
+			ue.Suci = "testsuci"
+			ue.Supi = supi
+			ue.Pei = "testpei"
+			ue.State = amfContext.ContextSetup
+			ue.MacFailed = false
+			ue.Tai = ue.RanUe.Tai
+			ue.SecurityContextAvailable = true
+			ue.NgKsi.Ksi = 1
+			key := [16]uint8{0x0D, 0x0E, 0x0A, 0x0D, 0x0B, 0x0E, 0x0E, 0x0F, 0x0F, 0x0E, 0x0E, 0x0D, 0x0C, 0x0A, 0x0F, 0x0E}
+			algo := security.AlgCiphering128NEA2
+			ue.KnasEnc = key
+			ue.KnasInt = key
+			ue.CipheringAlg = algo
+			ue.IntegrityAlg = security.AlgIntegrity128NIA0
+
+			registrationRequest, err := buildTestRegistrationRequestMessage(algo, &key, ue.ULCount.Get())
+			if err != nil {
+				t.Fatalf("could not build registration request message: %v", err)
+			}
+
+			ue.RegistrationRequest = registrationRequest.RegistrationRequest
+
+			ue.RegistrationType5GS = tc
+			if tc == nasMessage.RegistrationType5GSMobilityRegistrationUpdating {
+				ue.RegistrationRequest.Capability5GMM = &nasType.Capability5GMM{}
+			}
+
+			m := buildTestIdentityResponseMessage()
+
+			err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+			if err != nil {
+				t.Fatalf("expected no errors but got: %v", err)
+			}
+
+			if len(ngapSender.SentDownlinkNASTransport) != 1 {
+				t.Fatalf("should have sent a Downlink NAS Transport message")
+			}
+
+			resp := ngapSender.SentDownlinkNASTransport[0]
+			nm := new(nas.Message)
+			nm.SecurityHeaderType = nas.GetSecurityHeaderType(resp.NasPdu) & 0x0f
+
+			payload := make([]byte, len(resp.NasPdu))
+			copy(payload, resp.NasPdu)
+			payload = payload[7:]
+
+			if nm.SecurityHeaderType != nas.SecurityHeaderTypeIntegrityProtectedAndCiphered {
+				t.Fatalf("expected a protected and ciphered NAS message")
+			}
+
+			if err := security.NASEncrypt(ue.CipheringAlg, ue.KnasEnc, ue.ULCount.Get(), security.Bearer3GPP, security.DirectionDownlink, payload); err != nil {
+				t.Fatalf("could not decrypt NAS message: %v", err)
+			}
+
+			err = nm.PlainNasDecode(&payload)
+			if err != nil {
+				t.Fatalf("could not decode ciphered NAS message: %v", err)
+			}
+
+			if nm.GmmHeader.GetMessageType() != nas.MsgTypeRegistrationAccept {
+				t.Fatalf("expected a registration accept message, got '%v'", nm.GmmHeader.GetMessageType())
+			}
+		})
+	}
+}
+
+func TestHandleIdentityResponse_ContextSetup_Error(t *testing.T) {
+	testcases := []uint8{
+		nasMessage.RegistrationType5GSInitialRegistration,
+		nasMessage.RegistrationType5GSMobilityRegistrationUpdating,
+		nasMessage.RegistrationType5GSPeriodicRegistrationUpdating,
+	}
+
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+			supi := "imsi-001019756139935"
+			amf := &amfContext.AMF{
+				DBInstance: &FakeDBInstance{},
+				Ausf: &FakeAusf{
+					AvKgAka: &models.Av5gAka{
+						Rand: hex.EncodeToString(make([]byte, 16)),
+						Autn: hex.EncodeToString(make([]byte, 16)),
+					},
+					Supi:  supi,
+					Kseaf: "testkey",
+				},
+				UEs: make(map[string]*amfContext.AmfUe),
+			}
+
+			ue, ngapSender, err := buildUeAndRadio()
+			if err != nil {
+				t.Fatalf("could not create UE and radio: %v", err)
+			}
+
+			ue.Suci = "testsuci"
+			ue.Supi = supi
+			ue.Pei = "testpei"
+			ue.State = amfContext.ContextSetup
+			ue.MacFailed = false
+			ue.Tai = ue.RanUe.Tai
+			ue.SecurityContextAvailable = true
+			ue.NgKsi.Ksi = 1
+			key := [16]uint8{0x0D, 0x0E, 0x0A, 0x0D, 0x0B, 0x0E, 0x0E, 0x0F, 0x0F, 0x0E, 0x0E, 0x0D, 0x0C, 0x0A, 0x0F, 0x0E}
+			algo := security.AlgCiphering128NEA2
+			ue.KnasEnc = key
+			ue.KnasInt = key
+			ue.CipheringAlg = algo
+			ue.IntegrityAlg = security.AlgIntegrity128NIA0
+
+			registrationRequest, err := buildTestRegistrationRequestMessage(algo, &key, ue.ULCount.Get())
+			if err != nil {
+				t.Fatalf("could not build registration request message: %v", err)
+			}
+
+			ue.RegistrationRequest = registrationRequest.RegistrationRequest
+
+			ue.RegistrationType5GS = tc
+			if tc == nasMessage.RegistrationType5GSMobilityRegistrationUpdating {
+				ue.RegistrationRequest.Capability5GMM = &nasType.Capability5GMM{}
+			}
+
+			m := buildTestIdentityResponseMessage()
+
+			err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+			if err == nil {
+				t.Fatalf("expected error but got none")
+			}
+
+			if len(ngapSender.SentDownlinkNASTransport) != 0 {
+				t.Fatalf("should not have sent a Downlink NAS Transport message")
+			}
+
+			if ue.State != amfContext.Deregistered {
+				t.Fatalf("ue should have transitioned to Deregistered state, but got: %v", ue.State)
+			}
+		})
+	}
+}
+
+func TestHandleIdentityResponse_IdentityError(t *testing.T) {
+	testcases := []amfContext.StateType{amfContext.Authentication, amfContext.ContextSetup}
+
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+			supi := "imsi-001019756139935"
+			amf := &amfContext.AMF{
+				DBInstance: &FakeDBInstance{},
+				Ausf: &FakeAusf{
+					AvKgAka: &models.Av5gAka{
+						Rand: hex.EncodeToString(make([]byte, 16)),
+						Autn: hex.EncodeToString(make([]byte, 16)),
+					},
+					Supi:  supi,
+					Kseaf: "testkey",
+				},
+				UEs: make(map[string]*amfContext.AmfUe),
+			}
+
+			ue, ngapSender, err := buildUeAndRadio()
+			if err != nil {
+				t.Fatalf("could not create UE and radio: %v", err)
+			}
+
+			ue.State = tc
+
+			m := buildTestIdentityResponseMessage()
+			m.SetMobileIdentityContents([]uint8{})
+			m.IdentityResponse.SetLen(0)
+
+			expected := "error handling identity response: mobile identity is empty"
+
+			err = handleIdentityResponse(context.TODO(), amf, ue, m.IdentityResponse)
+			if err == nil {
+				t.Fatalf("expected error but got none")
+			}
+
+			if err.Error() != expected {
+				t.Fatalf("expected error: %v, got %v", expected, err)
+			}
+
+			if len(ngapSender.SentDownlinkNASTransport) != 0 {
+				t.Fatalf("should not have sent a Downlink NAS Transport message")
+			}
+		})
+	}
+}
+
+func buildTestIdentityResponseMessage() *nas.GmmMessage {
+	m := nas.NewGmmMessage()
+
+	identityResponse := nasMessage.NewIdentityResponse(0)
+	identityResponse.SetExtendedProtocolDiscriminator(nasMessage.Epd5GSMobilityManagementMessage)
+	identityResponse.SetSpareHalfOctet(0x00)
+	identityResponse.SetMessageType(nas.MsgTypeIdentityResponse)
+	identityResponse.SetLen(18)
+	identityResponse.SetMobileIdentityContents([]uint8{nasMessage.MobileIdentity5GSTypeSuci, 0x00, 0xf1, 0x10, 0x10, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+
+	m.IdentityResponse = identityResponse
+	m.SetMessageType(nas.MsgTypeIdentityResponse)
+
+	return m
 }
