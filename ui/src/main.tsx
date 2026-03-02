1bfa0946f8ea1afe72915ae9fd918380ca78eb0e
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-03-02 09:19:02 -0500

    chore(ui): use snackbar in auth pages (#1054)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router";
+import { SnackbarProvider } from "./contexts/SnackbarContext";
 import "./globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppRouter />
+      <SnackbarProvider>
+        <AppRouter />
+      </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
