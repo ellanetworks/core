a96b1b572f15155de66482d8dec6d51fd38bfe19
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-04-06 08:18:31 -0400

    feat: decode pdu_session_resource_setup_response_transfer
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/decoder/ngap/pdu_session_resource_setup_response.go
+++ internal/decoder/ngap/pdu_session_resource_setup_response.go
@@ -1,19 +1,49 @@
 package ngap
 
 import (
+	"encoding/binary"
 	"fmt"
 
+	"github.com/ellanetworks/core/internal/decoder/utils"
+	"github.com/free5gc/aper"
 	"github.com/free5gc/ngap/ngapType"
 )
 
+type DLQosFlowPerTNLInformation struct {
+	GTPTunnel          GTPTunnel           `json:"gtp_tunnel"`
+	AssociatedQosFlows []AssociatedQosFlow `json:"associated_qos_flows"`
+}
+
+type AssociatedQosFlow struct {
+	QosFlowIdentifier int64 `json:"qos_flow_identifier"`
+}
+
+type QosFlowFailedToSetupItem struct {
+	QosFlowIdentifier int64                   `json:"qos_flow_identifier"`
+	Cause             utils.EnumField[uint64] `json:"cause"`
+}
+
+type PDUSessionResourceSetupResponseTransferDecoded struct {
+	DLQosFlowPerTNLInformation DLQosFlowPerTNLInformation `json:"dl_qos_flow_per_tnl_information"`
+	QosFlowFailedToSetupList   []QosFlowFailedToSetupItem `json:"qos_flow_failed_to_setup_list,omitempty"`
+}
+
+type PDUSessionResourceSetupUnsuccessfulTransferDecoded struct {
+	Cause utils.EnumField[uint64] `json:"cause"`
+}
+
 type PDUSessionResourceSetupSURes struct {
-	PDUSessionID                            int64  `json:"pdu_session_id"`
-	PDUSessionResourceSetupResponseTransfer []byte `json:"pdu_session_resource_setup_response_transfer"`
+	PDUSessionID                            int64                                           `json:"pdu_session_id"`
+	PDUSessionResourceSetupResponseTransfer *PDUSessionResourceSetupResponseTransferDecoded `json:"pdu_session_resource_setup_response_transfer,omitempty"`
+
+	Error string `json:"error,omitempty"`
 }
 
 type PDUSessionResourceFailedToSetupSURes struct {
-	PDUSessionID                                int64  `json:"pdu_session_id"`
-	PDUSessionResourceSetupUnsuccessfulTransfer []byte `json:"pdu_session_resource_setup_unsuccessful_transfer"`
+	PDUSessionID                                int64                                               `json:"pdu_session_id"`
+	PDUSessionResourceSetupUnsuccessfulTransfer *PDUSessionResourceSetupUnsuccessfulTransferDecoded `json:"pdu_session_resource_setup_unsuccessful_transfer,omitempty"`
+
+	Error string `json:"error,omitempty"`
 }
 
 func buildPDUSessionResourceSetupResponse(pduSessionResourceSetupResponse ngapType.PDUSessionResourceSetupResponse) NGAPMessageValue {
@@ -71,11 +101,18 @@ func buildPDUSessionResourceSetupListSUResIE(pduList ngapType.PDUSessionResource
 
 	for i := 0; i < len(pduList.List); i++ {
 		item := pduList.List[i]
+		entry := PDUSessionResourceSetupSURes{
+			PDUSessionID: item.PDUSessionID.Value,
+		}
 
-		pduSessionList = append(pduSessionList, PDUSessionResourceSetupSURes{
-			PDUSessionID:                            item.PDUSessionID.Value,
-			PDUSessionResourceSetupResponseTransfer: item.PDUSessionResourceSetupResponseTransfer,
-		})
+		transfer, err := decodeSetupResponseTransfer(item.PDUSessionResourceSetupResponseTransfer)
+		if err != nil {
+			entry.Error = fmt.Sprintf("failed to decode response transfer: %v", err)
+		} else {
+			entry.PDUSessionResourceSetupResponseTransfer = transfer
+		}
+
+		pduSessionList = append(pduSessionList, entry)
 	}
 
 	return pduSessionList
@@ -86,12 +123,82 @@ func buildPDUSessionResourceFailedToSetupListSUResIE(pduList ngapType.PDUSession
 
 	for i := 0; i < len(pduList.List); i++ {
 		item := pduList.List[i]
-
-		pduSessionList = append(pduSessionList, PDUSessionResourceFailedToSetupSURes{
+		entry := PDUSessionResourceFailedToSetupSURes{
 			PDUSessionID: item.PDUSessionID.Value,
-			PDUSessionResourceSetupUnsuccessfulTransfer: item.PDUSessionResourceSetupUnsuccessfulTransfer,
-		})
+		}
+
+		transfer, err := decodeSetupUnsuccessfulTransfer(item.PDUSessionResourceSetupUnsuccessfulTransfer)
+		if err != nil {
+			entry.Error = fmt.Sprintf("failed to decode unsuccessful transfer: %v", err)
+		} else {
+			entry.PDUSessionResourceSetupUnsuccessfulTransfer = transfer
+		}
+
+		pduSessionList = append(pduSessionList, entry)
 	}
 
 	return pduSessionList
+}
+
+func decodeSetupResponseTransfer(transfer aper.OctetString) (*PDUSessionResourceSetupResponseTransferDecoded, error) {
+	if transfer == nil {
+		return nil, fmt.Errorf("transfer is nil")
+	}
+
+	pdu := &ngapType.PDUSessionResourceSetupResponseTransfer{}
+
+	err := aper.UnmarshalWithParams(transfer, pdu, "valueExt")
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal response transfer: %v", err)
+	}
+
+	result := &PDUSessionResourceSetupResponseTransferDecoded{}
+
+	dlInfo := &pdu.DLQosFlowPerTNLInformation
+	if dlInfo.UPTransportLayerInformation.GTPTunnel != nil {
+		tunnel := dlInfo.UPTransportLayerInformation.GTPTunnel
+		teid := binary.BigEndian.Uint32(tunnel.GTPTEID.Value)
+		addr := tunnel.TransportLayerAddress.Value.Bytes
+		ip := fmt.Sprintf("%d.%d.%d.%d", addr[0], addr[1], addr[2], addr[3])
+
+		result.DLQosFlowPerTNLInformation.GTPTunnel = GTPTunnel{
+			GTPTEID:               teid,
+			TransportLayerAddress: ip,
+		}
+	}
+
+	for _, flow := range dlInfo.AssociatedQosFlowList.List {
+		result.DLQosFlowPerTNLInformation.AssociatedQosFlows = append(
+			result.DLQosFlowPerTNLInformation.AssociatedQosFlows,
+			AssociatedQosFlow{QosFlowIdentifier: flow.QosFlowIdentifier.Value},
+		)
+	}
+
+	if pdu.QosFlowFailedToSetupList != nil {
+		for _, flow := range pdu.QosFlowFailedToSetupList.List {
+			result.QosFlowFailedToSetupList = append(result.QosFlowFailedToSetupList, QosFlowFailedToSetupItem{
+				QosFlowIdentifier: flow.QosFlowIdentifier.Value,
+				Cause:             causeToEnum(flow.Cause),
+			})
+		}
+	}
+
+	return result, nil
+}
+
+func decodeSetupUnsuccessfulTransfer(transfer aper.OctetString) (*PDUSessionResourceSetupUnsuccessfulTransferDecoded, error) {
+	if transfer == nil {
+		return nil, fmt.Errorf("transfer is nil")
+	}
+
+	pdu := &ngapType.PDUSessionResourceSetupUnsuccessfulTransfer{}
+
+	err := aper.UnmarshalWithParams(transfer, pdu, "valueExt")
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal unsuccessful transfer: %v", err)
+	}
+
+	return &PDUSessionResourceSetupUnsuccessfulTransferDecoded{
+		Cause: causeToEnum(pdu.Cause),
+	}, nil
 }
