80e2d9fa058c5fedb6c2e38707a2db8d3073156b
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2026-07-16 13:57:35 -0400

    fix: Clean path name in pergen

--- cmd/pergen/output.go
+++ cmd/pergen/output.go
@@ -3,10 +3,21 @@
 
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func init() {
-	writeFile = func(filename string, data []byte) error {
-		return os.WriteFile(filename, data, 0o600)
+	writeFile = func(_, filename string, data []byte) error {
+		cleaned := filepath.Clean(filename)
+
+		if strings.Contains(cleaned, "..") {
+			return fmt.Errorf("output path escapes source directory: %s", filename)
+		}
+
+		return os.WriteFile(cleaned, data, 0o600)
 	}
 }
