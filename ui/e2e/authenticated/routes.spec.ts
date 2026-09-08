7aa035c827a29783c81610b497c2f2dec54ba25f
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-09-08 15:33:47 -0400

    chore: further cleanup
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- ui/e2e/authenticated/routes.spec.ts
+++ ui/e2e/authenticated/routes.spec.ts
@@ -5,16 +5,13 @@ import { test, expect, type Page } from "@playwright/test";
 import { assertNoA11yViolations } from "../a11y";
 import { PRODUCT } from "../../src/utils/product";
 
-const startsWith = (value: string) =>
-  new RegExp(`^${value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}`);
-
 const ERROR_BOUNDARY = "Something went wrong in the interface";
 
 const ROUTES = [
   {
     label: "Dashboard",
     route: "/dashboard",
-    heading: startsWith(PRODUCT.name),
+    heading: new RegExp(`^${PRODUCT.name}`),
   },
   { label: "Subscribers", route: "/subscribers", heading: /^Subscribers/ },
   { label: "Profiles", route: "/profiles", heading: /^Profiles/ },
