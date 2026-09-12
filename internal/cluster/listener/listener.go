0acb7ecc26d7c226081df2fba7c5d0c65a179968
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-09-12 09:56:13 -0400

    chore: remove code comments
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/cluster/listener/listener.go
+++ internal/cluster/listener/listener.go
@@ -300,9 +300,6 @@ func (l *Listener) AdvertiseAddress() string {
 	return l.cfg.AdvertiseAddress
 }
 
-// BoundAddress returns the address the listener is actually bound to,
-// or "" before Start succeeds. Unlike AdvertiseAddress it reflects the
-// kernel-assigned port when BindAddress requested port 0.
 func (l *Listener) BoundAddress() string {
 	l.mu.Lock()
 	defer l.mu.Unlock()
