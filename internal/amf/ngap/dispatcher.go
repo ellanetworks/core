6c7d2c544a421671172df215ad864846d7f00b0e
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-09-15 07:41:03 -0400

    chore(o11y): set attributes at span creation time
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/amf/ngap/dispatcher.go
+++ internal/amf/ngap/dispatcher.go
@@ -15,6 +15,7 @@ import (
 	"github.com/ellanetworks/core/internal/logger"
 	"github.com/ellanetworks/core/internal/sctp"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
@@ -49,6 +50,11 @@ func Dispatch(ctx context.Context, amfInstance *amf.AMF, conn *sctp.SCTPConn, ms
 
 	ctx, span := tracer.Start(ctx, "ngap/receive",
 		trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithAttributes(
+			attribute.Int("ngap.message_size", len(msg)),
+			attribute.String("network.protocol.name", "ngap"),
+			attribute.String("network.transport", "sctp"),
+		),
 	)
 	defer span.End()
 
